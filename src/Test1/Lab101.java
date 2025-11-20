b2afd84e20dfe8346b394bd906d7bc2ff47aa278
napat68 <aoridea13@gmail.com>
2025-11-20 20:20:13 +0700

    First commit

---
+++ src/Test1/Lab101.java
@@ -0,0 +1,20 @@
+package Test1;
+
+import java.util.Scanner;
+
+		public class Lab101 {
+		    public static void main(String[] args) {
+
+		        Scanner sc = new Scanner(System.in);
+
+		        int a = sc.nextInt();
+		        int b = sc.nextInt();
+
+		        int sum = a + b;
+
+		        System.out.println(sum);
+
+		        sc.close();
+		    }
+
+	}

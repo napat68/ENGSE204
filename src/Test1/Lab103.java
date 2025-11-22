d944b0431221cb92707f7c23604da9d5080c3c46
napat68 <aoridea13@gmail.com>
2025-11-22 16:19:50 +0700

    update

--- src/Test1/Lab103.java
+++ src/Test1/Lab103.java
@@ -6,19 +6,18 @@ public class Lab103 {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);  // Get input from keyboard
+        Scanner Number = new Scanner(System.in);  
 
-        System.out.print("Enter number: ");
-        int num = sc.nextInt();               // Read numeric from user
+        System.out.print("Please enter number: ");
+        int num = Number.nextInt();               
         
-        // Checked even or odd
-        if (num % 2 == 0) {                   // If divisible by 2-> even
-            System.out.println("Even");
-        } else {                              // If not divisible by 2-> odd
+        if (num % 2 == 1) {                   
             System.out.println("Odd");
+        } else {                              
+            System.out.println("Even");
         }
 
-        sc.close();
+        Number.close();
     }
 }
 

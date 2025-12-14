27ab02d8e627e57bac78ca64d0411d22ed92ed70
napat68 <aoridea13@gmail.com>
2025-12-14 14:05:13 +0700

    update

--- Lab3/Lab301/Lab306.java
+++ Lab3/Lab301/Lab306.java
@@ -1,14 +1,65 @@
 package Lab301;
 
-public class Lab306 {
+import java.util.Scanner;
 
-	public Lab306() {
-		// TODO Auto-generated constructor stub
-	}
+class BankAccount {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+ private double balance;
 
-	}
+ public BankAccount(double initialBalance) {
+     if (initialBalance >= 0) {
+         this.balance = initialBalance;
+     } else {
+         this.balance = 0;
+     }
+ }
 
+ public double getBalance() {
+     return this.balance;
+ }
+
+ public void deposit(double amount) {
+     if (amount > 0) {
+         this.balance += amount;
+         System.out.println("Deposit successful.");
+         return; 
+     }
+     System.out.println("Invalid deposit amount.");
+ }
+
+ public void withdraw(double amount) {
+     if (amount <= 0) {
+         System.out.println("Invalid withdrawal amount.");
+         return; 
+     }
+
+     if (amount <= this.balance) {
+         this.balance -= amount;
+         System.out.println("Withdrawal successful.");
+         return; 
+     }
+
+     System.out.println("Insufficient funds.");
+ }
 }
+
+public class Lab306 {
+ public static void main(String[] args) {
+
+     Scanner input = new Scanner(System.in);
+
+     double initialBalance = input.nextDouble();
+     double depositAmount  = input.nextDouble();
+     double withdrawAmount = input.nextDouble();
+
+     BankAccount acc = new BankAccount(initialBalance);
+
+     acc.deposit(depositAmount);
+     acc.withdraw(withdrawAmount);
+
+     System.out.println("Final Balance: " + acc.getBalance());
+
+     input.close();
+ }
+}
+

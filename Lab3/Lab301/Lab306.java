83e31d5f940d84e309f267bff2d59fa12869c361
napat68 <aoridea13@gmail.com>
2025-12-14 14:00:47 +0700

    add

---
+++ Lab3/Lab301/Lab306.java
@@ -0,0 +1,14 @@
+package Lab301;
+
+public class Lab306 {
+
+	public Lab306() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
